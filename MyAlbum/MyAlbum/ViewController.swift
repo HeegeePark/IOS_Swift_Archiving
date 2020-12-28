f32d3dc4705d61593bea5d9b2c725343095cb30b
heegeepark <phg980513@naver.com>
2020-12-28 18:32:17 +0900

    Update MyAlbum_Outlett

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue = 0
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        priceLabel.text = "₩ \(currentValue)"
     }
     
     @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 ₩\(currentValue) 입니다."
+        
         // UIAlertController으로 alert 생성
-        let alert = UIAlertController(title: "Hello", message: "My first App!!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         
         // UIAlertAction으로 얼럿에 대한 액션 정의
         // 따로 다른 조치 취하지 않으니 핸들러는 nil
@@ -27,8 +35,10 @@ class ViewController: UIViewController {
         // present로 얼럿 띄우기
         present(alert, animated: true, completion: nil)
         
+        let randomPrice = arc4random_uniform(10000) + 1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "₩ \(currentValue)"
     }
     
-
 }
 
