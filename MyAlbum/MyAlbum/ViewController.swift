7ceebe7a2d35ba4558e3ba63f09128163e2d6511
heegeepark <phg980513@naver.com>
2020-12-28 18:58:38 +0900

    Refactor MyAlbum_closure

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 타입추론 하는법 : option키를 누른 상태로 변수 클릭하면 볼 수 있음.
     var currentValue = 0
     
     // 레이블을 아울렛 연결
@@ -24,18 +25,15 @@ class ViewController: UIViewController {
         
         // UIAlertController으로 alert 생성
         let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
-        
         // UIAlertAction으로 얼럿에 대한 액션 정의
         // 따로 다른 조치 취하지 않으니 핸들러는 nil
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in self.refresh()
+        })
         // 정의한 액션을 만들었던 얼럿에 추가
         alert.addAction(action)
-        
         // present로 얼럿 띄우기
         present(alert, animated: true, completion: nil)
-        
-        refresh()
+ 
     }
     
     // 가격 초기화
