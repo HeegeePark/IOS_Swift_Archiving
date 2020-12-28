4047f7a6239cc7aead706191ccd6884b87265212
heegeepark <phg980513@naver.com>
2020-12-28 17:18:48 +0900

    Add MyAlbum

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 박희지 on 2020/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func hello(_ sender: Any) {
+        // 버튼 클릭 시 실행됨.
+        print("hello")
+    }
+    
+
+}
+
