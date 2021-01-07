b1a816e9539a5aebef3a5b1ba60ac9a162e41eca
heegeepark <phg980513@naver.com>
2021-01-07 16:21:45 +0900

    Feat: Pass data in Modal add func prepare(), updateUI()

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -12,6 +12,18 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
     let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
     
+    // segue 수행되기 직전 준비하는 함수
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // -> DetailViewController 데이터 줄 거
+        if segue.identifier == "showDetail" {
+            let vc = segue.destination as? DetailViewController
+            if let index = sender as? Int {
+                vc?.name = nameList[index]
+                vc?.bounty = bountyList[index]
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -41,7 +53,7 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     // 클릭이 되면 어떻게 응답할 것인지
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // showDetail segue 실행
-        performSegue(withIdentifier: "showDetail", sender: nil)
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
     }
 }
 
