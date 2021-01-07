d6bba9c35ec1abe61f575fa23624a45a100c5df5
heegeepark <phg980513@naver.com>
2021-01-07 13:34:45 +0900

    Feat: TabelView add images customize listCell

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,12 +21,19 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     // UITableViewDataSource
     // cell 몇 개를 띄울건지, 어떻게 띄울건지
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return nameList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        // guard ... else : ... 이 nil이면 else 구문 실행
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
         
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imageView?.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
         return cell
     }
     
@@ -32,4 +42,11 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("-->\(indexPath)")
     }
+}
+
+// custom cell
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
 }
