1c8cc7f16123629b3336faf7abab2ef7eb431bf2
heegeepark <phg980513@naver.com>
2021-01-07 12:13:12 +0900

    Feat: TabelView

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -7,23 +7,29 @@
 
 import UIKit
 
-class BountyViewController: UIViewController {
-
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // UITableViewDataSource
+    // cell 몇 개를 띄울건지, 어떻게 띄울건지
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    // UITableViewDelegate
+    // 클릭이 되면 어떻게 응답할 것인지
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("-->\(indexPath)")
+    }
 }
