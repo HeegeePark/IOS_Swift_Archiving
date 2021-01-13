a9a6e77d94c67303a7b2c047a30b155bd5dc3a71
heegeepark <phg980513@naver.com>
2021-01-13 23:52:16 +0900

    Feat: Animation add animations using layouts of DetailViewController

--- BountyList/BountyList/DetailViewController.swift
+++ BountyList/BountyList/DetailViewController.swift
@@ -28,12 +28,65 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
     
+    // 뷰가 메모리에 올라온 시점에 호출
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    // 뷰가 실행된 이후 호출
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+        
+    }
+    
+    private func prepareAnimation() {
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    private func showAnimation() {
+        // constant가 0 = 가운데에 있음
+        // 레이아웃의 값 변경됨.
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+        // withDuration: 얼마동안 일어날 것인가?
+        // delay: 애니메이션이 일어나기 전 얼마나 딜레이 할지
+        // options: 애니메이션 어떤 효과?
+        // animations: 애니메이션 실행 부분
+        // completion: 애니메이션이 끝난 후 할 부분
+//        UIView.animate(withDuration: 0.3,
+//                       delay: 0.1,
+//                       options: .curveEaseIn,
+//                       animations: {
+//            self.view.layoutIfNeeded()
+//        }, completion: nil)
+        
+        // 스프링같이 통통뛰는 효과 줄 수 있음
+        // layoutIfNeeded(): 레이아웃을 다시 조정해야하면 하렴
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.1,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 2,
+                       options: .allowUserInteraction,
+                       animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        UIView.transition(with: imgView,
+                          duration: 0.3,
+                          options: .transitionFlipFromLeft,
+                          animations: nil,
+                          completion: nil
+        )
     }
     
     func updateUI() {
