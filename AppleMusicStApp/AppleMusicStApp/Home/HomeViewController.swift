2de332abd77701a2fa6692c6a2cf5cbad0813b45
heegeepark <phg980513@naver.com>
2021-01-27 00:16:19 +0900

    Feat: CollectionView HeaderView update headerview

--- AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
+++ AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
@@ -41,7 +41,21 @@ extension HomeViewController: UICollectionViewDataSource {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
             // TODO: 헤더 구성하기
-            return UICollectionReusableView()
+            guard let item = trackManager.todaysTrack else {
+                return UICollectionReusableView()
+            }
+            
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TrackCollectionHeaderView", for: indexPath) as? TrackCollectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            header.update(with: item)
+            header.tapHandler = { item -> Void in
+                // Player 띄우기
+                print("---> item title: \(item.convertToTrack()?.title)")
+            }
+            
+            return header
         default:
             return UICollectionReusableView()
         }
