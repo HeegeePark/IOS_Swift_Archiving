0fb8e4dacbdb6e18b661571c52efe68f224a1556
heegeepark <phg980513@naver.com>
2021-03-25 04:02:51 +0900

    Feat: URLSession update playground json -> struct 파싱

--- Playgrounds/Networking/URLSession_Basic.playground/Contents.swift
+++ Playgrounds/Networking/URLSession_Basic.playground/Contents.swift
@@ -17,14 +17,42 @@ let config = URLSessionConfiguration.default
 let session = URLSession(configuration: config)
 
 // URL Components
-var urlComponents = URLComponents(string: "https://github.com/HeegeePark?")!
-let tabQuery = URLQueryItem(name: "tab", value: "overview")
-let fromQuery = URLQueryItem(name: "from", value: "2020-12-01")
-let toQuery = URLQueryItem(name: "to", value: "2020-12-31")
-urlComponents.queryItems?.append(tabQuery)
-urlComponents.queryItems?.append(fromQuery)
-urlComponents.queryItems?.append(toQuery)
+var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+let mediaQuery = URLQueryItem(name: "media", value: "music")
+let entityQuery = URLQueryItem(name: "entity", value: "song")
+let termQuery = URLQueryItem(name: "term", value: "지드래곤")
+urlComponents.queryItems?.append(mediaQuery)
+urlComponents.queryItems?.append(entityQuery)
+urlComponents.queryItems?.append(termQuery)
 let requestURL = urlComponents.url!
+
+// structs
+struct Response: Codable {
+    let resultCount: Int
+    let tracks: [Track]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case tracks = "results"
+        
+    }
+}
+
+struct Track: Codable {
+    let title: String
+    let artistName: String
+    let thumbnailPath: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case artistName
+        case thumbnailPath = "artworkUrl30"
+    }
+    
+    // trackname
+    // artistName
+    // artworkUrl30
+}
 
 let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
     guard error == nil else { return }
@@ -40,7 +68,34 @@ let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
     guard let resultData = data else { return }
     let resultString = String(data: resultData, encoding: .utf8)
     
-    print("---> result: \(resultString)")
+    // 목표: 트랙리스트 오브젝트로 가져오기
+    
+    // 하고 싶은 일 목록
+    // - Data -> Track 목록으로 가져오 고 싶음 [Track]
+    // - Track 오브젝트 만들어야 겠다
+    // - Data에서 struct로 파싱하고 싶다 > Codable 이용해서 만들자
+    //  - Json 파일, 데이터 > 오브젝트 (Codable 이용)
+    //  - Response, Track 이렇게 두개 만들어야지
+    
+    // 해야할 일
+    // - Response, Track struct
+    // - struct의 프로퍼티 이름과 실제 데이터의 키와 맞추기 (Codable 디코딩 위해)
+    // - 파싱하기 (Decoding)
+    // - 트랙리스트 가져오기
+    
+    
+    // 파싱 및 트랙 가져오기
+    do {
+        let decoder = JSONDecoder()
+        let response = try decoder.decode(Response.self, from: resultData)
+        let tracks = response.tracks
+        
+        print("---> tracks: \(tracks.count)")
+        print("---> last: \(tracks.last?.title), \(tracks.last?.thumbnailPath)")
+    } catch let error {
+        print("---> error: \(error.localizedDescription)")
+    }
+//    print("---> result: \(resultString)")
 }
 
 // dataTask 실행
