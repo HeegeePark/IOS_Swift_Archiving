6bf81f32f24e3ca1b72b5f2f4ab1fb208d8ae002
heegeepark <phg980513@naver.com>
2021-04-10 00:38:42 +0900

    Feat: Cell Resize cell 크기 정렬

--- NaverMovieSt/NaverMovieSt/MovieViewController.swift
+++ NaverMovieSt/NaverMovieSt/MovieViewController.swift
@@ -14,33 +14,43 @@ class MovieViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    
-
 
 }
 
 extension MovieViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return movies.count
+//        return movies.count
+        return 9
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else { return UICollectionViewCell() }
-        let movie = movies[indexPath.item]
+//        let movie = movies[indexPath.item]
         
         return cell
     }
+}
+
+// cell을 클릭했을 때 이벤트 작성
+extension MovieViewController: UICollectionViewDelegate {
     
     
 }
 
-extension MovieViewController: UICollectionViewDelegate {
-    
+// collectionView cell 크기 조정
+extension MovieViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width * 10/7
+        return CGSize(width: width, height: height)
+    }
 }
 
 class ResultCell: UICollectionViewCell {
-    
+    @IBOutlet weak var movieThumbnail: UIImageView!
 }
 
 // Model movie
